bf4adabe8ed9b806cba8ae9a4fa9a622a321824c
Vickitar <arencibiav@gmail.com>
2022-01-23 21:21:11 -0500

    completion of course module # 2

--- Rstudio project area.R
+++ Rstudio project area.R
@@ -1,5 +1,18 @@
 #testing? 
 #testing this again
 
+# Assignment 2 was provided to us by Dr Friedman. The values of c() (also knowns a concatenate) are assigned
+# to "assignment2." What this means is that anytime we type out or use assignment2 as the variable it will
+# automatically populate the numbers inside of c(). This allows us to concatenate lots of data into a single
+# variable consolidating our information. 
 assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
-myMean <- function(assignment2) { return(sum(assignment2)/length(assignment2)) }
+
+#myMean is a function we have created. The variable myMean has been asssigned as a function. What this allows us
+# to group instructions together that take inputs and uses them to compute other values to return a result. 
+
+myMean <- function(assignment2) { 
+  return(sum(assignment2)/length(assignment2)) 
+}
+#here we can calculate the result of our function & our variable resulting in the answer of 19.25
+
+myMean(assignment2)
